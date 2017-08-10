bdce86378cb6932a8ff6707dbd844470af5c6b05
Sami Kangas <zumartic@gmail.com>
2017-08-10 11:00:28 +0300

    About page updated

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
     return (
             <div>
-                <h3>About</h3>
-                <p>Welcome to about page</p>
+                <h1 className="text-centered">About</h1>
+                <p>This is a weather application build on React. This has been build as a part of the Complete React Wev App Developer Course.</p>
+                <p>Some tools that I have used</p>
+                <ul>
+                    <li>
+                        <a href="https://facebook.github.io/react">React</a> - Used JavaScrip framework.
+                    </li>
+                    <li>
+                        <a href="http://openweathermap.org">Open Weather Map</a> - This is used for searthing the weather by city name.
+                    </li>
+                </ul>
             </div>
         );
 }
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -18,7 +18,7 @@ var WeatherForm = React.createClass({
                     <input type="text" ref="location" />
                 </div>
                 <div>
-                    <button>Get Weather</button>
+                    <button className="button">Get Weather</button>
                 </div>
             </form> 
         );
